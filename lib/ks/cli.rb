cedd40c17e3197dfd154ab2d8ae5844132741be5
dniman <dade.niman@gmail.com>
2018-06-05 17:09:58 +0300

    new command

--- lib/ks/cli.rb
+++ lib/ks/cli.rb
@@ -1,4 +1,13 @@
+require 'thor'
+require 'ks/generators/working_directory'
+
 module KS
-  class CLI
+  class CLI < Thor
+
+    desc "new [NAME]", "Generate a new ks working directory"
+    def new(*args)
+      KS::Generators::WorkingDirectory.start(args)
+    end
+
   end
 end  
--- spec/ks/cli_spec.rb
+++ spec/ks/cli_spec.rb
@@ -1,4 +1,24 @@
 require "spec_helper"
+require 'tmpdir'
 
 RSpec.describe KS::CLI do
+
+  describe "#new" do
+    it "creates new working directory" do
+      args, opts = Thor::Options.split(["new","working_directory"])
+      config = {}
+
+      tmp = Dir.mktmpdir('working_directory')
+      cwd = Dir.pwd
+      Dir.chdir(tmp)
+
+      instance = described_class.new(args,opts,config)
+      expect(capture(:stdout) { instance.new("working_directory") }).to eq("      create  working_directory\n")
+
+      Dir.chdir(cwd)
+      FileUtils.rm_rf(tmp)
+    end
+
+  end
+
 end
