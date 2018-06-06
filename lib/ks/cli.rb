214c162b4685520203e3982376af7de220b09807
dade <dade@hp>
2018-06-06 22:28:20 +0300

    introduced .app_root

--- lib/ks/cli.rb
+++ lib/ks/cli.rb
@@ -4,8 +4,24 @@ require 'ks/generators/working_directory'
 module KS
   class CLI < Thor
 
+    class << self
+      attr_reader :app_root
+
+      def start(given_args = ARGV, config={})
+        @app_root = config[:app_root]
+        super
+      end
+    end
+
     desc "new [NAME]", "Generate a new ks working directory"
     def new(*args)
+
+      msg = <<~MSG
+        Can't generate a new working directory within the directory of another, please change to a non-working directory first.
+        For details run: ks --help
+      MSG
+
+      raise Error,msg if self.class.app_root
       KS::Generators::WorkingDirectory.start(args)
     end
 
