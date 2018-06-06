412eb4ef7c86a7945f2c4dec8ada92927936e467
dade <dade@hp>
2018-06-06 22:32:06 +0300

    new example under context

--- spec/ks/cli_spec.rb
+++ spec/ks/cli_spec.rb
@@ -1,24 +1,36 @@
 require "spec_helper"
-require 'tmpdir'
 
 RSpec.describe KS::CLI do
-
   describe "#new" do
-    it "creates new working directory" do
-      args, opts = Thor::Options.split(["new","working_directory"])
-      config = {}
+    
+    let(:args) { Thor::Options.split(["new","working_directory"])[0] }
+    let(:opts) { Thor::Options.split(["new","working_directory"])[1] }
+    let(:config) { Hash.new }
 
-      tmp = Dir.mktmpdir('working_directory')
-      cwd = Dir.pwd
-      Dir.chdir(tmp)
-
-      instance = described_class.new(args,opts,config)
-      expect{ instance.new("working_directory") }.to output("      create  working_directory\n").to_stdout
-
-      Dir.chdir(cwd)
-      FileUtils.rm_rf(tmp)
+    context "when .app_root is unset" do  
+      include_context "working-directory"
+      
+      it "creates new working directory" do
+        instance = described_class.new(args,opts,config)
+        expect{ instance.new("working_directory") }.to output("      create  working_directory\n").to_stdout
+      end
     end
 
+    context "when .app_root is set" do
+      include_context "working-directory"
+ 
+      it "prints error message" do
+        instance = described_class.new(args,opts,config)
+        allow(described_class).to receive(:app_root).and_return(Dir.mktmpdir)
+
+        msg = <<~MSG
+          Can't generate a new working directory within the directory of another, please change to a non-working directory first.
+          For details run: ks --help
+        MSG
+        
+        expect{ instance.new("working_directory") }.to raise_error(Thor::Error, msg)
+      end
+    end
   end
 
 end
